6b0db6b7b2466585059633800dcc3f703c38a4a9
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2012-12-03 19:17:20 +0700

    add tab with hidden swipe gesture, also fix actionbar image

--- src/com/android/settings/MainSettings.java
+++ src/com/android/settings/MainSettings.java
@@ -19,12 +19,18 @@ package com.android.settings;
 import com.android.settings.R;
 
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.os.SystemProperties;
 import android.app.TabActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.TabHost;
@@ -33,7 +39,6 @@ import android.widget.TabHost.TabSpec;
 
 public class MainSettings extends TabActivity {
 
-	private static TabHost mTabHost;
 	private Intent intent;
     private ActionBar mActionBar;
 
@@ -57,8 +62,6 @@ public class MainSettings extends TabActivity {
                   }
         });
 
-		mTabHost = (TabHost) findViewById(android.R.id.tabhost);
-		mTabHost.getTabWidget().setDividerDrawable(null);
 		Intent intent; // Reusable Intent for each tab
 
 		intent = new Intent().setClass(MainSettings.this, Settings.class);
@@ -125,7 +128,67 @@ public class MainSettings extends TabActivity {
 		setupTab(new TextView(this), getString(R.string.about_misc_settings), intent);
     }
 
+    public static class FlingableTabHost extends TabHost {
+        GestureDetector mGestureDetector;
+
+        Animation mRightInAnimation;
+        Animation mRightOutAnimation;
+        Animation mLeftInAnimation;
+        Animation mLeftOutAnimation;
+
+        public FlingableTabHost(Context context, AttributeSet attrs) {
+            super(context, attrs);
+
+            mRightInAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_right_in);
+            mRightOutAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_right_out);
+            mLeftInAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_left_in);
+            mLeftOutAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_left_out);
+
+            final int minScaledFlingVelocity = ViewConfiguration.get(context)
+                    .getScaledMinimumFlingVelocity() * 10; // 10 = fudge by experimentation
+
+            mGestureDetector = new GestureDetector(new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                        float velocityY) {
+                    int tabCount = getTabWidget().getTabCount();
+                    int currentTab = getCurrentTab();
+                    if (Math.abs(velocityX) > minScaledFlingVelocity &&
+                        Math.abs(velocityY) < minScaledFlingVelocity) {
+
+                        final boolean right = velocityX < 0;
+                        final int newTab = MathUtils.constrain(currentTab + (right ? 1 : -1),
+                                0, tabCount - 1);
+                        if (newTab != currentTab) {
+                            // Somewhat hacky, depends on current implementation of TabHost:
+                            // http://android.git.kernel.org/?p=platform/frameworks/base.git;a=blob;
+                            // f=core/java/android/widget/TabHost.java
+                            View currentView = getCurrentView();
+                            setCurrentTab(newTab);
+                            View newView = getCurrentView();
+
+                            newView.startAnimation(right ? mRightInAnimation : mLeftInAnimation);
+                            currentView.startAnimation(
+                                    right ? mRightOutAnimation : mLeftOutAnimation);
+                        }
+                    }
+                    return super.onFling(e1, e2, velocityX, velocityY);
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(MotionEvent ev) {
+            if (mGestureDetector.onTouchEvent(ev)) {
+                return true;
+            }
+            return super.onInterceptTouchEvent(ev);
+        }
+    }
+
 	private void setupTab(final View view, final String tag, final Intent myIntent) {
+
+                final TabHost mTabHost = getTabHost();
 
 		View tabview = createTabView(mTabHost.getContext(), tag);
 		TabSpec setContent =  mTabHost.newTabSpec(tag).setIndicator(tabview).setContent(myIntent);
